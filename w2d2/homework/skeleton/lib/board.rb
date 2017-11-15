0dc52b5897f7549b23af3e877b0e6cc76f915bbb
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-11-14 21:02:00 -0500

    adding rspec homework

--- w2d2/homework/skeleton/lib/board.rb
+++ w2d2/homework/skeleton/lib/board.rb
@@ -1,51 +1,38 @@
-require_relative 'player'
-require 'byebug'
 class Board
-
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
     @cups = place_stones
-    @player1 = Player.new(name1, 1)
-    @player2 = Player.new(name2, 2)
   end
 
-
   def place_stones
-    arr = Array.new(14) {Array.new(4) {:stone}}
-    arr[6] = []
-    arr[13] = []
-    arr
+    cups = Array.new(14) {Array.new}
+    cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times { cup << :stone}
+    end
+    cups
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" unless start_pos.between?(1,14)
-    raise "Invalid starting cup" unless start_pos != 13
-    raise "Invalid starting cup" unless start_pos != 6
+    raise "Invalid starting cup" unless start_pos.between?(1,12)
   end
 
   def make_move(start_pos, current_player_name)
-    current_player = get_player(current_player_name)
     stones = @cups[start_pos].dup
     @cups[start_pos] = []
-
-    while stones.length > 0
-      current_idx = start_pos + 1
-      next if current_player.side == 1 && current_idx == 6
-      next if current_player.side == 2 && current_idx == 13
-      @cups[current_idx] << stones.pop
+    stones.each_with_index do |stone, idx|
+      next if current_player_name == @name1 && start_pos+idx+1 == 13
+      next if current_player_name == @name2 && start_pos+idx+1 == 6
+      start_pos = -1 if start_pos+idx
+      @cups[start_pos+idx+1] << :stone
     end
-
-
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-  end
-
-  def get_player(name)
-    return @player1 if name == @player1.name
-    return @player2 if name == @player2.name
   end
 
   def render
